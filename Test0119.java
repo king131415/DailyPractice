7c47a5e7e9606e40daffc2c6f0212113102176ce
king <1292203381@qq.com>
2021-04-06 19:34:30 +0800

    日常练习

---
+++ Test0119.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Test0119 {
+    public static void main(String[] args) {
+//        char [] str={'h','e','l','l','o'};
+//        String s=new String(str);
+//        System.out.println(s);
+//        String[] a={"544","646"};
+//        System.out.println(a.length);
+//        for(int i=0;i<a.length;i++){
+//            System.out.print(a[i]+' ');
+//
+//            String str="hello";
+//            for(int j=0;j<100;j++){
+//                str+=j;
+//            }
+//        }
+        String s="123";
+        s="456";
+        System.out.println(s);
+    }
+}
