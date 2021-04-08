31b824a0a0d40cd52193b47a20dbbce23ef34b9e
king <1292203381@qq.com>
2021-04-08 15:56:30 +0800

    日常练习

---
+++ ShuzuTest.java
@@ -0,0 +1,20 @@
+public class ShuzuTest {
+    public static void main(String[] args) {
+//        实现一个方法 avg, 以数组为参数, 求数组中所有元素的平均值(注意方法的返回值类型)
+//        int[] arr={1,2,3,4,5,6,7,8,9};
+//       double result= Avg(arr);
+
+        int i=10;
+       int b=i++;
+
+        System.out.println(b);
+
+    }
+//    public static double Avg(int[] arr) {
+//        double tempResult=0;
+//        for(int i=0;i<arr.length;i++){
+//            tempResult+=arr[i];
+//        }
+//        return tempResult;
+//    }
+}
