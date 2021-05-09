c9edbec39f63eec98dd75463be5eeed5415f8934
king <1292203381@qq.com>
2021-05-09 20:55:26 +0800

    日常练习

---
+++ Menu.java
@@ -0,0 +1,9 @@
+package ThreeGobangGame;
+
+public class Menu {
+    public Menu() {
+        System.out.println("**********************");
+        System.out.println("*1.开始游戏   2.退出游戏*");
+        System.out.println("**********************");
+    }
+}
