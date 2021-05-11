be22c918fdc46e3db5dbddef034ab05655297d9a
king <1292203381@qq.com>
2021-05-11 21:53:50 +0800

    servlet学习

---
+++ Test01.java
@@ -0,0 +1,9 @@
+package javaDailyProctice.Test0511;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test01 {
+    public final  static Map< String,Servlet> SERVLET_CONTAINER=new HashMap(8);
+
+}
