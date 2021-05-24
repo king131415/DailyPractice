d8eaf921ad69d8434e947e24b64583e27a20b944
king <1292203381@qq.com>
2021-05-24 22:13:43 +0800

    日常练习

---
+++ 最大数/最大数/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(void)
+{
+	int a[] = { 12, 22, 36, 45, 65 };
+	printf("%d \n", a[0]);
+	printf("%d \n", a[1]);
+	printf("%d \n", a[2]);
+	printf("%d \n", a[3]);
+	printf("%d \n", a[4]);
+	
+}
