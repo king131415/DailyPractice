243768daf752995898cd0cbb1f1fe6ae321debe0
king <1292203381@qq.com>
2021-05-24 22:28:49 +0800

    日常练习

---
+++ 最大值2.0/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+
+    int data[] = {55, 59, 69, 100, 109};
+       for(int i = 0;i < 5;i++)
+    {
+        printf("%d\n", data[i]);
+    }
+
+
+}
