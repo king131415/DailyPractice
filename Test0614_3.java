1d60eed906a983c06f07f4fcd80293dba57d4f67
king <1292203381@qq.com>
2021-06-14 23:58:32 +0800

    日常练习

---
+++ Test0614_3.java
@@ -0,0 +1,19 @@
+package javaDailyProctice.Test0614;
+
+public class Test0614_3 {
+    // 2   1   6   4    5    3
+    //2    7   3   1     1
+    // 5   1   3   1     5    9     10
+    public int findMaxGap(int[] A, int n) {
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
