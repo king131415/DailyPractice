48e021935dbaa9fd2c4341c2f0f681a326011eba
king <1292203381@qq.com>
2021-06-19 23:41:57 +0800

    日常练习

---
+++ Test0619_1.java
@@ -0,0 +1,18 @@
+package javaDailyProctice.Test0619_1;
+
+public class Test0619_1 {
+    public static void main(String[] args) {
+//        String s="a"+new String("b");
+//        System.out.println(s);
+        int a=10;
+        int b=20;
+
+        System.out.println(Integer.MAX_VALUE<<1);
+
+//
+//
+//           10100
+//           10 1000
+
+    }
+}
