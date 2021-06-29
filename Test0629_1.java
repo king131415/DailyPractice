c15e824bc69bc7af5fdd7367dd117635ff236047
king <1292203381@qq.com>
2021-06-29 22:08:53 +0800

    日常练习

---
+++ Test0629_1.java
@@ -0,0 +1,11 @@
+package javaDailyProctice.Test0629_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test0629_1 {
+    public static void main(String[] args) {
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+    }
+}
