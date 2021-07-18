20de08b200e1bd019a2128cd9dc824b172321cca
king <1292203381@qq.com>
2021-07-18 23:47:24 +0800

    日常练习

---
+++ Test0718_2.java
@@ -0,0 +1,28 @@
+package javaDailyProctice.Test0718;
+
+import java.util.Scanner;
+
+/**
+ * 题目描述
+ *
+ * 输入任意个字符串，将其中的小写字母变为大写，大写字母变为小写，其他字符不用处理；
+ *
+ * 输入描述:
+ * 任意字符串：abcd12#%XYZ
+ *
+ * 输出描述:
+ * 输出字符串：ABCD12#%xyz
+ *
+ * 示例1：
+ * 输入： abcd12#%XYZ
+ * 输出： ABCD12#%xyz
+ */
+
+public class Test0718_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+
+
+    }
+}
