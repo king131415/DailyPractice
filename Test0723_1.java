5817a9af0a0fccc6f9b6d3d9c787670ddc11f755
king <1292203381@qq.com>
2021-07-23 23:56:12 +0800

    日常练习

---
+++ Test0723_1.java
@@ -0,0 +1,46 @@
+package javaDailyProctice.Test0723;
+
+
+import java.util.Scanner;
+
+/**
+ * 二进制1的个数
+ */
+public class Test0723_1 {
+    //  3 ==>11   3%2  1  1  1       1    1000
+//                                   1    0111
+    // 1000  11   10                 7    111
+      ////        11      01         6    110
+    //1                   00
+    // 1 00000 11
+    // 1 11111 00
+    // 1 11111 0 1
+
+    /**
+     *
+     *
+     */
+    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//
+//        int num = 0, flag = 1;
+//        while (flag != 0) {
+//            if ((n & flag) != 0) {
+//                num++;
+//            }
+//            flag <<= 1;
+//        }
+//        System.out.println(num);
+
+
+
+
+    }
+
+    //-3    0 0000000 001010
+    //      1
+    //
+}
+
+
