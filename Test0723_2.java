a806479116e11b0a678631175f1602b28469e508
king <1292203381@qq.com>
2021-07-23 23:59:18 +0800

    日常练习

---
+++ Test0723_2.java
@@ -0,0 +1,20 @@
+package javaDailyProctice.Test0723;
+
+import java.util.Scanner;
+
+public class Test0723_2 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//
+//        int num = 0, flag = 1;
+//        while (flag != 0) {
+//            if ((n & flag) != 0) {
+//                num++;
+//            }
+//            flag <<= 1;
+//        }
+//        System.out.println(num);
+    }
+}
