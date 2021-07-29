14db7161a614d894bfc9e9f4f4ea5d5a8190ab93
king <1292203381@qq.com>
2021-07-29 23:55:27 +0800

    日常练习

---
+++ Test0729_1.java
@@ -0,0 +1,26 @@
+package javaDailyProctice.Test0616;
+
+public class Test0729_1 {
+    protected int x=10;
+
+    public Test0616_1() {
+        System.out.println(x);
+        change();
+        System.out.println(x);
+    }
+    public void change(){
+        x=3;
+    }
+}
+class b extends Test0616_1{
+    protected  int b=1;
+    @Override
+    public void change() {
+        x=9;
+    }
+
+    public static void main(String[] args) {
+        b b1=new b();
+        System.out.println(b1.x);
+    }
+}
